5f083e8ee89ba71cb8f92d1837cc30e26490be69
Dylan Sweetensen <dylansweetensen@gmail.com>
2018-01-12 22:19:22 +1300

    creating an html interface component

--- src/components/pure/WaveForm/InterfaceLayer/InterfaceLayer.js
+++ src/components/pure/WaveForm/InterfaceLayer/InterfaceLayer.js
@@ -1,1 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import layer from 'interface-layer.css';
+
+class InterfaceLayer extends Component {
+  constructor(props) {
+    super(props);
+
+    this.setState({
+      isDragging: false,
+      dragStart: 0,
+    });
+
+    this.onMouseDown = this.onMouseDown.bind(this);
+    this.onMouseUp = this.onMouseUp.bind(this);
+  }
+
+  onMouseDown(event) {
+    const { target: { xOffset } } = event;
+
+    this.setState({
+      isDragging: true,
+      dragStart: xOffset,
+    });
+  }
+
+  onMouseUp(event) {
+    if (!this.state.isDragging) {
+      return;
+    }
+
+    const { target: { xOffset } } = event;
+
+    this.setState({
+      isDragging: false,
+    });
+
+    const distance = xOffset - this.state.dragStart;
+
+    this.props.onInterfaceDrag(distance)
+  }
+
+  render() {
+    return <div className={layer.interface} onMouseDown={this.onMouseDown} onMouseUp={this.onMouseUp} />
+  }
+}
+
+export default InterfaceLayer;
